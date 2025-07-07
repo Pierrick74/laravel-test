4f6de6c98084b419f259c9e851f8b2d768141729
pierrick <pierrickviret@gmail.com>
2025-07-07 12:02:11 +0200

    Feat(route) : 2.1 creation du routeur

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome', ['laravel_version' => app()->version(), 'php_version' => phpversion()]);
+    return "Homepage";
+});
+
+Route::get('/product', function () {
+    return "Liste des produits";
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit {$id}";
+});
+
+Route::get('/cart', function () {
+    return "Panier";
 });
