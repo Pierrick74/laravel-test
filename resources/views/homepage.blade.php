de3df2e6bf959c48a20452224ca0c7a76a481cfe
pierrick <pierrickviret@gmail.com>
2025-07-08 13:59:56 +0200

    Feat(homepage) : add filter

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.default')
 @section('content')
     <h1>Attrapez les tous !</h1>
-    <div class="input-group rounded">
+    <div class="input-group rounded border border-3 border-dark">
         <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
         <button class="input-group-text border-0" id="search-addon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-            </svg>
+            <img src="assets/search.svg" alt="">
         </button>
     </div>
-
-    <section id="Card" class="mt-4 d-flex flex-wrap gap-3">
-        <div class="card" style="width: 18rem;">
+    <div class="d-flex gap-3 flex-fill mt-2 p-2 border-3  border rounded-pill scroll_h">
+    <button  class="btn btn-block rounded-pill w-100" >etat</button>
+    <button  class="btn btn-block rounded-pill w-100">Prix </button>
+    <button  class="btn btn-block rounded-pill w-100">Certifiaction</button>
+    <button  class="btn btn-block rounded-pill w-100">Illustrateur</button>
+    </div>
+    <section id="Card" class="mt-4 d-flex flex-wrap gap-3 justify-content-center">
+        <div class="card border-1 border-dark" style="width: 18rem;">
             <div class="d-block text-center h-75 d-inline-block">
-                <img src="assets/photos/dacaufeu.webp" class="card-img-top mw-75" alt="...">
+                <img src="../assets/photos/dacaufeu.webp" class="card-img-top mw-75" alt="...">
             </div>
             <div class="card-body">
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Nom:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">Dracaufeu</p></span>
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Extension:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">Promo</p></span>
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Nb d'exemplaire:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">2</p></span>
-                <boutton onclick="window.location='{{ url("/product/Dracaufeu") }}'" class="btn btn-block ">Détail</boutton>
+                    <boutton onclick="window.location='{{ url("/product/Dracaufeu") }}'" class="btn btn-block w-100">Détail</boutton>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
+        <div class="card border-1 border-dark" style="width: 18rem;">
             <div class="d-block text-center h-75 d-inline-block">
                 <img src="assets/photos/picachu.webp" class="card-img-top mw-75" alt="...">
             </div>
@@ -31,7 +34,7 @@
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Nom:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">Picachu</p></span>
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Extension:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">Epée et bouclier</p></span>
                 <span class="d-flex"><p class="card-title w-50 font-weight-bold">Nb d'exemplaire:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">3</p></span>
-                <a href="#" class="btn btn-block ">Détail</a>
+                <a href="#" class="btn btn-block w-100">Détail</a>
             </div>
         </div>
     </section>
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,2 @@
 <p>© 2025 Pokemarket</p>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
