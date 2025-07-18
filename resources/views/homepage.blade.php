2bc90ddfec5ce91c66f9cc2de012f13f05678f76
pierrick <pierrickviret@gmail.com>
2025-07-18 11:55:30 +0200

    refact(filter): update display

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.default')
 @section('content')
-
-
     <div class="d-flex justify-content-center">
-    <div class=" d-inline-block">
-        <h1>Attrapez les tous !</h1>
-        <div class="input-group rounded border border-3 border-dark">
-            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-            <button class="input-group-text border-0" id="search-addon">
-                <img src="assets/search.svg" alt="">
-            </button>
-        </div>
-    <div class="d-flex gap-3 flex-fill mt-2 p-2 border-3  border rounded-pill scroll_h justify-content-center">
-    <button onclick="window.location='{{ url("/sort/name") }}'" class="btn btn-block rounded-pill">nom</button>
-    <button onclick="window.location='{{ url("/sort/extension") }}'" class="btn btn-block rounded-pill">extention </button>
-    <button onclick="window.location='{{ url("/sort/PV") }}'" class="btn btn-block rounded-pill">PV</button>
-    <button onclick="window.location='{{ url("/sort/Type") }}'" class="btn btn-block rounded-pill">Type</button>
-    </div>
-    <section id="Card" class="mt-4 d-flex flex-wrap gap-3 justify-content-center">
-        @foreach($cards as $card)
-            <div class="card border-1 border-dark" style="width: 18rem;">
-                <div class="d-block text-center h-75 d-inline-block">
-                    <img src="../assets/photos/{{$card -> photo}}" class="card-img-top w-50 mt-2" alt="...">
+        <div class="d-inline-block">
+            <h1 class="p-4">Attrapez les tous !</h1>
+            <div class="input-group rounded border border-3 border-dark">
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                <button class="input-group-text border-0" id="search-addon">
+                    <img src="assets/search.svg" alt="">
+                </button>
+            </div>
+            <div class="container mt-2 p-2 justify-content-center filter">
+                <div class="row column-gap-3 gap-1 ">
+                    <button onclick="window.location='{{ url("/sort/name") }}'" class="col btn btn-block rounded-pill">nom</button>
+                    <button onclick="window.location='{{ url("/sort/extension") }}'" class="col btn btn-block rounded-pill">extention </button>
+                    <div class="w-100 d-md-none"></div>
+                    <button onclick="window.location='{{ url("/sort/PV") }}'" class="col btn btn-block rounded-pill">PV</button>
+                    <button onclick="window.location='{{ url("/sort/Type") }}'" class="col btn btn-block rounded-pill">Type</button>
                 </div>
+            </div>
+            <section id="Card" class="mt-4 d-flex flex-wrap gap-3 justify-content-center">
+                @foreach($cards as $card)
+                    <div class="card border-1 border-dark" style="width: 18rem;">
+                        <div class="d-block text-center h-75 d-inline-block">
+                            <img src="../assets/photos/{{$card -> photo}}" class="card-img-top w-50 mt-2" alt="...">
+                        </div>
                 <div class="card-body">
                     <span class="d-flex"><p class="card-title w-50 font-weight-bold">Nom:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">{{$card -> name}}</p></span>
                     <span class="d-flex"><p class="card-title w-50 font-weight-bold">Extension:</p><p class="m-0 px-2 w-50 text-right font-weight-bold">{{$card -> extension}}</p></span>
